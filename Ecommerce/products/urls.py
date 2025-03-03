f40289fd48f2bde3f5fbae713317b60dadd46ed1
Nandanalinson <nandanalinson7@gmail.com>
2025-03-04 00:21:45 +0530

    First commit

--- Ecommerce/products/urls.py
+++ Ecommerce/products/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from django.conf import settings
+from django.urls import path 
+from django.conf import settings 
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('products.urls')),
+    path('', views.index, name='home'),
+    path('products_list', views.list_products, name='product_list'),
+    path('product_detail', views.detail_product, name='product_detail'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
